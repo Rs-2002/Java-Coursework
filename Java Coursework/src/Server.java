683eceb0301d6825e38cef79613e1ae100231b42
Robert Spear <92854643+Rs-2002@users.noreply.github.com>
2022-10-28 17:26:29 +0100

    Adding Comments

--- Java Coursework/src/Server.java
+++ Java Coursework/src/Server.java
@@ -13,23 +13,23 @@ public class Server {
     public Server() throws FileNotFoundException {   
         
         nodes = new ArrayList<Node>();
-        java.io.File prc = new java.io.File("src/Data.csv");
+        java.io.File prc = new java.io.File("src/Data.csv"); // Creating the adventure map using a csv file containing all necessary data
         fileLink = new Scanner(prc);
-        while (hasAnotherLine()){
+        while (hasAnotherLine()){ // Repeats creating the arrayList of Nodes for each line in the csv
             String line = getLine();
 
             String[] lineArray = splitLine(line);
 
-            int NodeID=Integer.parseInt(lineArray[0]);
-            int yesID=Integer.parseInt(lineArray[1]);
-            int noID=Integer.parseInt(lineArray[2]);
+            int NodeID=Integer.parseInt(lineArray[0]); // The ID of the line used for identification when traversing
+            int yesID=Integer.parseInt(lineArray[1]);  // The reference for the ID of the Yes option 
+            int noID=Integer.parseInt(lineArray[2]); // The reference for the ID of the No option 
 
-            String description=(lineArray[3]);
-            String question=(lineArray[4]);
-            String option1 = (lineArray[5]);
-            String option2 = (lineArray[6]);
+            String description=(lineArray[3]); //String description of what is happening
+            String question=(lineArray[4]); //String question for the next decision
+            String option1 = (lineArray[5]); //String for one option of what to do next
+            String option2 = (lineArray[6]); //String for second option of what to do next
 
-            Node n = new Node(NodeID,yesID,noID,description,question,option1,option2);
+            Node n = new Node(NodeID,yesID,noID,description,question,option1,option2); //Adds the above information into a node
             nodes.add(n); 
         }
     }
@@ -54,7 +54,7 @@ public class Server {
 
     private String[] splitLine (String line){return line.split(",");}
 
-    
+    //finds and returns the description of the current node
     public String findIDReturnDescription(int currentNode){
 
         //loop through each item in arraylist
@@ -72,6 +72,7 @@ public class Server {
 
         return "NO MATCH FOUND";
     }
+    //finds and returns the description for option 2 of the current node
     public String findIDReturnOption1(int currentNode){
         for(Node node: nodes){
             int findID = node.getNodeID();
@@ -86,6 +87,7 @@ public class Server {
         }
         return "NO MATCH FOUND";
     }
+    //finds the description for option 2 of the current node
     public String findIDReturnOption2(int currentNode){
         for(Node node: nodes){
             int findID = node.getNodeID();
@@ -100,7 +102,7 @@ public class Server {
         }
         return "NO MATCH FOUND";
     }
-
+/////// findNextNode() used to find the next node in the story by getting the ID from the Yes/No ID of the current node
     public int findNextNode(int currentNode, int nextChoice){
 
         for(Node node:nodes){
